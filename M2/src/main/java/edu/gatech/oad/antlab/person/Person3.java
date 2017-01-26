f721e62473dabf96ce0a4957e2ecc3f4d36be910
gwang307 <gwang307@gatech.edu>
2017-01-26 12:59:43 -0500

    Person 3 was added

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -2,15 +2,15 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 3
  *  returns their name and a
- *  reversed string 
- *  
- *  @author  Bob
+ *  reversed string
+ *
+ *  @author  Gahan Wang
  *  @version 1.1
  */
 public class Person3 {
-   /** Holds the persons real name */  
+   /** Holds the persons real name */
 	private String name;
-	
+
 	/**
 	 * The constructor, takes in the persons
 	 * name
@@ -19,19 +19,19 @@ public class Person3 {
 	public Person3(String pname){
 	  name = pname;
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
 	  return name + calc(input);
 	}
-	
+
 	/**
 	 * This method should take the string
 	 * input and return its reverse.
@@ -41,8 +41,14 @@ public class Person3 {
 	 * @param input the string to be reversed
 	 * @return the reversed string
 	 */
-	private String calc(String input) {
+	private static String calc(String input) {
 	  //Person 3 put your implementation here
-	  return null;
+		int size = input.length();
+		System.out.println(size);
+		String aString = "";
+		for (int i = size; i >= 0; i--) {
+			aString += input.charAt(i);
+		}
+		return aString;
 	}
 }
